585fdcf085d158683a9c4d3e9731a9b4f864dd0a
Pollard <Pollard@fanruan.com>
2019-10-30 20:32:05 +0800

    初始化

---
+++ rpp-admin/src/main/java/com/byoryn/rppadmin/RppAdminApplication.java
@@ -0,0 +1,13 @@
+package com.byoryn.rppadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RppAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RppAdminApplication.class, args);
+    }
+
+}
---
+++ rpp-admin/src/test/java/com/byoryn/rppadmin/RppAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byoryn.rppadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RppAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
